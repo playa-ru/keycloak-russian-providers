9a32c3a8cfd069ee2a4937d02164e243952137c5
Anatolij Pohresnyj <a.k.pohresniy@gmail.com>
2019-02-07 16:41:38 +0800

    Логирование для Mail.Ru

--- src/main/java/ru/playa/keycloak/modules/mailru/MailRuIdentityProvider.java
+++ src/main/java/ru/playa/keycloak/modules/mailru/MailRuIdentityProvider.java
@@ -29,17 +29,17 @@ public class MailRuIdentityProvider
     /**
      * Запрос кода подтверждения.
      */
-    private static final String AUTH_URL = "https://oauth.mail.ru/login";
+    private static final String AUTH_URL = "https://connect.mail.ru/oauth/authorize";
 
     /**
      * Обмен кода подтверждения на токен.
      */
-    private static final String TOKEN_URL = "https://oauth.mail.ru/token";
+    private static final String TOKEN_URL = "https://connect.mail.ru/oauth/token";
 
     /**
      * Запрос информации о пользователе.
      */
-    private static final String PROFILE_URL = "https://oauth.mail.ru/userinfo";
+    private static final String PROFILE_URL = "http://www.appsmail.ru/platform/api";
 
     /**
      * Права доступа к данным пользователя по умолчанию.
@@ -122,6 +122,16 @@ public class MailRuIdentityProvider
     @Override
     protected BrokeredIdentityContext doGetFederatedIdentity(String accessToken) {
         try {
+
+            try {
+                logger.info(
+                    SimpleHttp.doGet("https://oauth.mail.ru/userinfo?access_token?access_token=" + accessToken,
+                                     session)
+                              .asJson());
+            } catch (Exception e) {
+                logger.error(e.getMessage(), e);
+            }
+
             String params = "app_id="
                     + getConfig().getClientId()
                     + "method=users.getInfosecure=1session_key="
@@ -140,9 +150,7 @@ public class MailRuIdentityProvider
 
             logger.info("url: " + url);
 
-            return extractIdentityFromProfile(null,
-                                              SimpleHttp.doGet(PROFILE_URL + "?access_token=" + accessToken, session)
-                                                        .asJson());
+            return extractIdentityFromProfile(null, SimpleHttp.doGet(url, session).asJson());
         } catch (IOException e) {
             throw new IdentityBrokerException("Could not obtain user profile from MailRu: " + e.getMessage(), e);
         }
